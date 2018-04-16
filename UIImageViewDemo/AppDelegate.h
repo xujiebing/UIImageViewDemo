44417b66bf02300fcbc9d7f0dabdc57de1af8799
xujiebing <xujiebing@bwton.com>
2018-04-16 22:35:55 +0800

    Initial Commit

---
+++ UIImageViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageViewDemo
+//
+//  Created by 徐结兵 on 2018/4/16.
+//  Copyright © 2018年 八维通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImageViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageViewDemo
+//
+//  Created by 徐结兵 on 2018/4/16.
+//  Copyright © 2018年 八维通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
